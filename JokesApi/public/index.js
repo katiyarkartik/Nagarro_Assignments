b320340313bf95c40bcc6d2c40d1b138e3bbe936
Kartik katiyar <kartikktyr@gmail.com>
2022-07-22 04:58:44 +0530

    data saved to mongodb

---
+++ JokesApi/public/index.js
@@ -0,0 +1,24 @@
+
+
+async function getJoke() {
+  const data = await fetch(" https://icanhazdadjoke.com/", {
+    headers: { Accept: "application/json" },
+  });
+  const obj = await data.json();
+  console.log(obj.joke);
+  document.getElementById("joke").innerHTML = obj.joke;
+}
+
+async function sendToBackend() {
+  const joke =  document.getElementById("joke")
+  const jokeContent = joke.innerText
+    const response = await fetch('/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({joke: jokeContent.toString()})
+    })
+    const data = await response.json()
+    console.log(data)
+}
--- JokesApi/server.js
+++ JokesApi/server.js
@@ -12,10 +12,22 @@ const jokesSchema = {
   joke: String,
 };
 const jokemodel = mongoose.model("joke", jokesSchema);
+
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html" );
+  res.sendFile(__dirname + "/index.html");
 });
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.post("/", (req, res) => {
+  let body = req.body;
+  let { joke } = body;
+  const newjoke = new jokemodel();
+  newjoke.joke = joke;
+  newjoke.save();
+});
+
 app.listen(3000, function () {
   console.log("running on port 3000...");
 });
