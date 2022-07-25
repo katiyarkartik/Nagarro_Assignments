4aad979b98efa49a228501968165c8c382c2d05b
Kartik katiyar <kartikktyr@gmail.com>
2022-07-25 17:42:54 +0530

    theme changer

---
+++ ThemeChanger/index.js
@@ -0,0 +1,24 @@
+function setblack() {
+  document.getElementById("con").style.backgroundColor = "black";
+  document.getElementById("t").style.borderColor = "white";
+  document.getElementById("t").style.backgroundColor = "black";
+  document.getElementById("t").style.color = "white";
+}
+function setblue() {
+    document.getElementById("con").style.backgroundColor = "blue";
+    document.getElementById("t").style.borderColor = "white";
+    document.getElementById("t").style.backgroundColor = "blue";
+    document.getElementById("t").style.color = "white";
+}
+function setgreen() {
+    document.getElementById("con").style.backgroundColor = "green";
+    document.getElementById("t").style.borderColor = "black";
+    document.getElementById("t").style.backgroundColor = "green";
+    document.getElementById("t").style.color = "black";
+}
+function setyellow() {
+    document.getElementById("con").style.backgroundColor = "yellow";
+    document.getElementById("t").style.borderColor = "black";
+    document.getElementById("t").style.backgroundColor = "yellow";
+    document.getElementById("t").style.color = "black";
+}
